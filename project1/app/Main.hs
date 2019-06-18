755588de42de672451be9a71f41d61f1fc6e913e
GlowDanBios <46843591+GlowDanBios@users.noreply.github.com>
2019-06-18 12:30:28 +0300

    mood asker

--- project1/app/Main.hs
+++ project1/app/Main.hs
@@ -3,4 +3,24 @@ module Main where
 import Lib
 
 main :: IO ()
-main = putStrLn "Hello guys"
+main = do
+  let add x y = x + y
+  let square x = x ** 2
+  let fact x = if x > 1
+               then x * fact(x-1)
+               else 1
+  print (add (square 7) 9)
+  print (fact 7)
+  print "Hello, how r u?"
+  mood <- getLine
+  if mood == "good"
+  then print "That's fine!"
+  else do
+       if mood == "bad"
+       then print "So sad (("
+       else do
+            print "Is it good?"
+            answer <- getLine
+            if answer == "yes"
+            then print "That's fine!"
+            else print "So sad(("
